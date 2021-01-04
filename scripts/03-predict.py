0567fb49efbc8f9f5679b79a8dcdd5798abf1452
FangyiChao <34549328+FannyChao@users.noreply.github.com>
2021-01-04 17:25:41 +0100

    Update 03-predict.py

--- scripts/03-predict.py
+++ scripts/03-predict.py
@@ -24,8 +24,6 @@ def main(in_folder, out_folder):
     # Create network
     model = ModelBCE(256, 192, batch_size=8)
     # Here need to specify the epoch of model sanpshot
-    #load_weights(model.net['output'], path= '/home/fchang/Bureau/salgan360_finetune/result_model/17_30/1730lfgen_', epochtoload=150, layernum=54)
-    #load_weights(model.net['output'], path= '/home/fchang/Bureau/saliency-salgan-2017/scripts/salgan360_model2/for_testing/ft45_18_model6_gen_', epochtoload=150, layernum=54)
     load_weights(model.net['output'], path= '/home/fchang/Bureau/salgan360_finetune/datasets/salient360_18/adft120/adft9_4_wei_5000_8/1845_120_gen_', epochtoload=40, layernum=54)
     #load_weights(model.net['output'], path= 'gen_',epochtoload=90, layernum=54)
     # Here need to specify the path to images and output path
